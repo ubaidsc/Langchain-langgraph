44cdc06dba5a44310b6015c3ca9f4bb238ddac85
Ubaid <ranaubaid409@gmail.com>
2025-07-08 16:43:26 +0500

    init

--- node_modules/langsmith/dist/utils/vercel.d.ts
+++
@@ -1,7 +0,0 @@
-import { KVMap } from "../schemas.js";
-export declare function extractUsageMetadata(span?: {
-    status?: {
-        code: number;
-    };
-    attributes?: Record<string, unknown>;
-}): KVMap;
